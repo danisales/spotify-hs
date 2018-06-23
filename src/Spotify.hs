f21fa62749802ea055e2e2d4e51d1306d8add10a
Danielle Sales <dso@cin.ufpe.br>
2018-06-23 12:07:21 -0300

    Add getTopTracks, getTrack and getTracks functions

--- src/Spotify.hs
+++ src/Spotify.hs
@@ -14,6 +14,9 @@ import Data.List
 import Secret
 import Types
 
+baseUrl :: String
+baseUrl = "https://api.spotify.com/v1"
+
 -- e.g. getArtist "6yz8H2Aks1bHaKNiHCutaR"
 getArtist :: String -> IO (Maybe Artist)
 getArtist id = do
@@ -21,7 +24,7 @@ getArtist id = do
   let opts = defaults & param "Accept" .~ ["application/json"]
                       & param "Content-Type" .~["application/json"]
                       & header "Authorization" .~ ["Bearer " <> B.pack token]
-  r <- getWith opts ("https://api.spotify.com/v1/artists/" <> id)
+  r <- getWith opts (baseUrl <> "/artists/" <> id)
   return $ (A.decode (fromJust $ r ^? responseBody) :: Maybe Artist)
 
 -- e.g. getArtists ["6yz8H2Aks1bHaKNiHCutaR", "4ERtgeBbWRkFzIz6LaFCeY", "3yY2gUcIsjMr8hjo51PoJ8"]
@@ -31,5 +34,46 @@ getArtists ids = do
   let opts = defaults & param "Accept" .~ ["application/json"]
                       & param "Content-Type" .~["application/json"]
                       & header "Authorization" .~ ["Bearer " <> B.pack token]
-  r <- getWith opts ("https://api.spotify.com/v1/artists?ids=" <> intercalate "," ids)
+  r <- getWith opts (baseUrl <> "/artists?ids=" <> intercalate "," ids)
   return $ (A.decode (fromJust $ r ^? responseBody) :: Maybe Artists)
+
+-- e.g. getRelatedArtists "6yz8H2Aks1bHaKNiHCutaR"
+getRelatedArtists :: String -> IO (Maybe Artists)
+getRelatedArtists id = do
+  token <- getAccessToken
+  let opts = defaults & param "Accept" .~ ["application/json"]
+                      & param "Content-Type" .~["application/json"]
+                      & header "Authorization" .~ ["Bearer " <> B.pack token]
+  r <- getWith opts (baseUrl <> "/artists/" <> id <> "/related-artists")
+  return $ (A.decode (fromJust $ r ^? responseBody) :: Maybe Artists)
+
+-- e.g. getTopTracks "6yz8H2Aks1bHaKNiHCutaR" "BR"
+getTopTracks :: String -> String -> IO (Maybe Tracks)
+getTopTracks id country = do
+  token <- getAccessToken
+  let opts = defaults & param "Accept" .~ ["application/json"]
+                      & param "Content-Type" .~["application/json"]
+                      & header "Authorization" .~ ["Bearer " <> B.pack token]
+  r <- getWith opts (baseUrl <> "/artists/" <> id <> "/top-tracks?country=" <> country)
+  return $ (A.decode (fromJust $ r ^? responseBody) :: Maybe Tracks)
+
+-- e.g. getTrack "0WQiDwKJclirSYG9v5tayI"
+getTrack :: String -> IO (Maybe Track)
+getTrack id = do
+  token <- getAccessToken
+  let opts = defaults & param "Accept" .~ ["application/json"]
+                      & param "Content-Type" .~["application/json"]
+                      & header "Authorization" .~ ["Bearer " <> B.pack token]
+  r <- getWith opts (baseUrl <> "/tracks/" <> id)
+  return $ (A.decode (fromJust $ r ^? responseBody) :: Maybe Track)
+
+-- e.g. getTracks ["2hopb3OJKYhhZ6L4ca9pBp", "3SVAN3BRByDmHOhKyIDxfC", "0WQiDwKJclirSYG9v5tayI"]
+getTracks :: [String] -> IO (Maybe Tracks)
+getTracks ids = do
+  token <- getAccessToken
+  let opts = defaults & param "Accept" .~ ["application/json"]
+                      & param "Content-Type" .~["application/json"]
+                      & header "Authorization" .~ ["Bearer " <> B.pack token]
+  r <- getWith opts (baseUrl <> "/tracks?ids=" <> intercalate "," ids)
+  return $ (A.decode (fromJust $ r ^? responseBody) :: Maybe Tracks)
+
--- src/Types.hs
+++ src/Types.hs
@@ -48,6 +48,14 @@ instance FromJSON ExternalURL where
   parseJSON = withObject "externalurl" $ \o ->
     ExternalURL <$> o .: "spotify"
 
+data ExternalID = ExternalID {
+  isrc :: String
+} deriving (Show)
+
+instance FromJSON ExternalID where
+  parseJSON = withObject "externalid" $ \o ->
+    ExternalID <$> o .: "isrc"
+
 data Followers = Followers {
   follower_href :: Maybe String,
   follower_total :: Integer
@@ -59,13 +67,133 @@ instance FromJSON Followers where
               <*> o .: "total"
 
 data Image = Image {
-  img_height :: Integer,
+  img_height :: Maybe Integer,
   img_url :: String,
-  img_width :: Integer
+  img_width :: Maybe Integer
 } deriving (Show)
 
 instance FromJSON Image where
-  parseJSON = withObject "followers" $ \o ->
-    Image <$> o .: "height"
+  parseJSON = withObject "image" $ \o ->
+    Image <$> o .:? "height"
           <*> o .: "url"
-          <*> o .: "width"
+          <*> o .:? "width"
+
+data Track = Track {
+  track_simplified_album :: SimplifiedAlbum,
+  track_simplified_artists :: [SimplifiedArtist],
+  track_available_markets :: [String],
+  track_disc_number :: Integer,
+  track_duration_ms :: Integer,
+  track_explict :: Bool,
+  track_external_ids :: ExternalID,
+  track_external_urls :: ExternalURL,
+  track_href :: String,
+  track_id :: String,
+  track_is_playable :: Maybe Bool,
+  track_linked_from :: Maybe String,
+  track_restriction :: Maybe Restriction,
+  track_name :: String,
+  track_popularity :: Integer,
+  track_preview_url :: Maybe String,
+  track_number :: Integer,
+  track_type :: String,
+  track_uri :: String
+} deriving (Show)
+
+instance FromJSON Track where
+  parseJSON = withObject "track" $ \o ->
+    Track <$> o .: "album"
+          <*> o .: "artists"
+          <*> o .: "available_markets"
+          <*> o .: "disc_number"
+          <*> o .: "duration_ms"
+          <*> o .: "explicit"
+          <*> o .: "external_ids"
+          <*> o .: "external_urls"
+          <*> o .: "href"
+          <*> o .: "id"
+          <*> o .:? "is_playable"
+          <*> o .:? "linked_from"
+          <*> o .:? "restrictions"
+          <*> o .: "name"
+          <*> o .: "popularity"
+          <*> o .:? "preview_url"
+          <*> o .: "track_number"
+          <*> o .: "type"
+          <*> o .: "uri"
+
+data Tracks = Tracks {
+  tracks :: [Track]
+} deriving (Show)
+
+instance FromJSON Tracks where
+  parseJSON = withObject "tracks" $ \o ->
+    Tracks <$> o .: "tracks"
+
+data Restriction = Restriction {
+  reason :: String
+} deriving (Show)
+
+instance FromJSON Restriction where
+  parseJSON = withObject "restriction" $ \o ->
+    Restriction <$> o .: "reason"
+
+data SimplifiedArtist = SimplifiedArtist {
+  s_artist_external_urls :: ExternalURL,
+  s_artist_href :: String,
+  s_artist_id :: String,
+  s_artist_name :: String,
+  s_artist_type :: String,
+  s_artist_uri :: String
+} deriving (Show)
+
+instance FromJSON SimplifiedArtist where
+  parseJSON = withObject "simplifiedartist" $ \o ->
+    SimplifiedArtist <$> o .: "external_urls"
+          <*> o .: "href"
+          <*> o .: "id"
+          <*> o .: "name"
+          <*> o .: "type"
+          <*> o .: "uri"
+
+data SimplifiedArtists = SimplifiedArtists {
+  s_artists :: [SimplifiedArtist]
+} deriving (Show)
+
+data SimplifiedAlbum = SimplifiedAlbum {
+  s_album_albumgroup :: Maybe String,
+  s_album_albumtype :: String,
+  s_album_artists :: [SimplifiedArtist],
+  s_album_available_markets :: [String],
+  s_album_external_urls :: ExternalURL,
+  s_album_href :: String,
+  s_album_id :: String,
+  s_album_images :: [Image],
+  s_album_name :: String,
+  s_album_release_date :: String,
+  s_album_release_date_precision :: String,
+  s_album_restrictions :: Maybe Restriction,
+  s_album_type :: String,
+  s_album_uri :: String
+} deriving (Show)
+
+instance FromJSON SimplifiedAlbum where
+  parseJSON = withObject "simplifiedalbum" $ \o ->
+    SimplifiedAlbum <$> o .:?: "album_group"
+          <*> o .: "album_type"
+          <*> o .: "artists"
+          <*> o .: "available_markets"
+          <*> o .: "external_urls"
+          <*> o .: "href"
+          <*> o .: "id"
+          <*> o .: "images"
+          <*> o .: "name"
+          <*> o .: "release_date"
+          <*> o .: "release_date_precision"
+          <*> o .:? "restrictions"
+          <*> o .: "type"
+          <*> o .: "uri"
+
+data SimplifiedAlbums = SimplifiedAlbums {
+  s_albums :: [SimplifiedAlbum]
+} deriving (Show)
