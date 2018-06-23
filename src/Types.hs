f21cbbabe7b8460db2a95970a8ac303eeacb1a94
Danielle Sales <dso@cin.ufpe.br>
2018-06-22 21:32:36 -0300

    Add Artist type

---
+++ src/Types.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Types where
+
+import Data.Aeson
+import Control.Lens
+import Data.Monoid
+
+data Artist = Artist {
+  artist_external_urls :: ExternalURL,
+  artist_followers :: Followers,
+  artist_genres :: [String],
+  artist_href :: String,
+  artist_id :: String,
+  artist_images :: [Image],
+  artist_name :: String,
+  artist_popularity :: Integer,
+  artist_type :: String,
+  artist_uri :: String
+} deriving (Show)
+
+instance FromJSON Artist where
+  parseJSON = withObject "artist" $ \o ->
+    Artist <$> o .: "external_urls"
+          <*> o .: "followers"
+          <*> o .: "genres"
+          <*> o .: "href"
+          <*> o .: "id"
+          <*> o .: "images"
+          <*> o .: "name"
+          <*> o .: "popularity"
+          <*> o .: "type"
+          <*> o .: "uri"
+
+data ExternalURL = ExternalURL {
+  spotify :: String
+} deriving (Show)
+
+instance FromJSON ExternalURL where
+  parseJSON = withObject "externalurl" $ \o ->
+    ExternalURL <$> o .: "spotify"
+
+data Followers = Followers {
+  follower_href :: Maybe String,
+  follower_total :: Integer
+} deriving (Show)
+
+instance FromJSON Followers where
+  parseJSON = withObject "followers" $ \o ->
+    Followers <$> o .:? "href"
+              <*> o .: "total"
+
+data Image = Image {
+  img_height :: Integer,
+  img_url :: String,
+  img_width :: Integer
+} deriving (Show)
+
+instance FromJSON Image where
+  parseJSON = withObject "followers" $ \o ->
+    Image <$> o .: "height"
+          <*> o .: "url"
+          <*> o .: "width"
