673b5075f8df6ba50c3886b3064c0b13b850244d
Danielle Sales <dso@cin.ufpe.br>
2018-06-25 09:40:44 -0300

    Use environment variables to get client id and client secret

--- src/Secret.hs
+++ src/Secret.hs
@@ -9,6 +9,7 @@ import qualified Data.ByteString.Char8 as B
 import Data.Monoid
 import Data.Maybe
 import Data.ByteString.Base64 as B64
+import System.Environment
 
 data Token = Token {access_token :: String,
                     token_type :: String,
@@ -23,19 +24,33 @@ instance FromJSON Token where
           <*> o .: "expires_in"
           <*> o .: "scope"
 
-clientID :: B.ByteString
-clientID = B.pack "SPOTIFY_CLIENT_ID"
+-- Get your credentials at: https://developer.spotify.com/dashboard
 
-clientSecret :: B.ByteString
-clientSecret = B.pack "SPOTIFY_CLIENT_SECRET"
+-- On terminal:
+-- export SPOTIFY_CLIENT_ID="YOUR CLIENT ID"
+-- export SPOTIFY_CLIENT_SECRET="YOUR CLIENT SECRET"
 
-authToken :: B.ByteString
-authToken = B64.encode $ clientID <> ":" <> clientSecret
+clientID :: IO (B.ByteString)
+clientID = do
+  id <- getEnv "SPOTIFY_CLIENT_ID"
+  return $ B.pack id
+
+clientSecret :: IO (B.ByteString)
+clientSecret = do
+  secret <- getEnv "SPOTIFY_CLIENT_SECRET"
+  return $ B.pack secret
+
+authToken :: IO (B.ByteString)
+authToken = do
+  id <- clientID
+  secret <- clientSecret
+  return $ B64.encode $ id <> ":" <> secret
 
 getToken :: IO (Maybe Token)
 getToken = do
+  token <- authToken
   let opts = defaults
-             & header "Authorization" .~ ["Basic " <> authToken]
+             & header "Authorization" .~ ["Basic " <> token]
   r <- postWith opts "https://accounts.spotify.com/api/token" ["grant_type" := ("client_credentials" :: String)]
   return $ (A.decode (fromJust $ r ^? responseBody) :: Maybe Token)
 
